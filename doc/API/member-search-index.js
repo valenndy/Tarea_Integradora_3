ac530783d03d489b6552497817ec6496e7318d7a
Andy <andyv2048@gmail.com>
2021-12-06 14:47:08 -0500

    Map and simulation

---
+++ doc/API/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ui","c":"Main","l":"answerOption(int)"},{"p":"ui","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"ui","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"ui","c":"Main","l":"showMenuAndGetOption()"}];updateSearchResults();
